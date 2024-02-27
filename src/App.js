09a966341b6c84a47eca4752260a9b4ca95b2361
hathim <hathim@mail.com>
2024-02-27 14:39:02 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+function TodoList() {
+  // State variable to store list of todos
+  const [todos, setTodos] = useState([]);
+  
+  // State variable to store the current todo input
+  const [todoInput, setTodoInput] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoInput.trim() !== '') {
+      // Add new todo to todos array
+      setTodos([...todos, todoInput]);
+      // Clear todo input
+      setTodoInput('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((item,idx) => idx !== index);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className='container'>
+      <h1>Todo List</h1>
+      </div>
+      <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <input className='search'
+          type="text" 
+          value={todoInput} 
+          onChange={(e) => setTodoInput(e.target.value)} 
+          placeholder="Enter new todo" 
+        />
+    
+        <button className='button' type="submit">Add</button>
+     
+      </form>
+      </div>
+      <div className='container'>
+      <ul>
+     
+        {/* Render list of todos */}
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+     
+            <button className='dltbtn' onClick={() => handleDelete(index)}>Delete</button>
+      
+          </li>
+         
+        ))}
+         
+      </ul>
+      </div>
+      </div>
+    // </div>
   );
-}
+} 
 
-export default App;
+export default TodoList;
+
+// export default Counter
